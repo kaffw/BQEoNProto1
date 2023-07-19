7872789a63be15bbc12ec38c05e03ba60918fbf2
kaffw <coroneljm01@gmail.com>
2023-07-19 20:47:41 +0800

    Meteor
    Meteor Animation
    Meteor Falling
    Meteor Spawning

---
+++ Terrain/Assets/MeteorFalling.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MeteorFalling : MonoBehaviour
+{
+    private Rigidbody2D meteorRB;
+    [SerializeField] public float fallingSpeed;
+
+    private void Start()
+    {
+        meteorRB = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        transform.position = new Vector2(meteorRB.transform.position.x - (Time.deltaTime * fallingSpeed), meteorRB.transform.position.y - (Time.deltaTime * fallingSpeed));
+    }
+}
---
+++ Terrain/Assets/MeteorSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawner : MonoBehaviour
+{
+    public GameObject meteorSpawn;
+    public float timer = 0f;
+    public float spawnInterval = 0f;
+    
+    void Update()
+    {
+        if (timer <= 10)
+        {
+            timer += Time.deltaTime;
+            spawnInterval += Time.deltaTime;
+        }
+
+        if (spawnInterval > 1f)
+        {
+            Instantiate(meteorSpawn, new Vector2(UnityEngine.Random.Range(20, 71), 50), transform.rotation);
+            spawnInterval = 0f;
+        }
+
+        else timer = 0f;
+    }
+}
