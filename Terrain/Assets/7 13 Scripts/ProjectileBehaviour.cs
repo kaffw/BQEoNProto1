c4001ec4c1fa7fae9fcd0f8cd06f8d00b85b55bc
kaffw <coroneljm01@gmail.com>
2023-07-21 00:35:39 +0800

    Naga
    Updates:
    Bulan Projectile
    In motion + burst
    Projectile burst in collision with Naga (theoretically all enemies --tagged Enemy--)
    
    Naga Update
    Idle with flipping
    Levitating

--- Terrain/Assets/7 13 Scripts/ProjectileBehaviour.cs
+++ Terrain/Assets/7 13 Scripts/ProjectileBehaviour.cs
@@ -4,27 +4,38 @@ using UnityEngine;
 
 public class ProjectileBehaviour : MonoBehaviour
 {
-    public float Speed = 10f;
+    public float Speed = 12.5f;
     //public Rigidbody2D rb;
     private Rigidbody2D bulletrb;
+    public Animator bulletAnim;
+
+    private bool freezeMotion = false;
 
     // Update is called once per frame
     private void Start()
     {
+        bulletAnim = GetComponent<Animator>();
         bulletrb = GetComponent<Rigidbody2D>();
         if (moveset.dirFire == true) bulletrb.velocity = Vector2.right * Speed;
         else bulletrb.velocity = Vector2.left * Speed;
-        Destroy(gameObject, 20f);
+        Destroy(gameObject, 15f);
     }
 
-
+    private void Update()
+    {
+        if (freezeMotion) bulletrb.velocity *= 0f;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        freezeMotion = true;
         var enemy = collision.collider.GetComponent<EnemyBehaviour>();
         if (enemy)
         {
             enemy.TakeHit(1);
         }
-        Destroy(gameObject);
+
+        bulletAnim.SetTrigger("ProjectileBurst");
+
+        Destroy(gameObject, 1.15f);
     }
 }
--- Terrain/Assets/NagaPathing.cs
+++ Terrain/Assets/NagaPathing.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class NagaPathing : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D nagaRB;
+    private Animator nagaAnim;
+
+    public GameObject Target;
+    public Vector2 TargetPos;
+
+    public bool faceDirection = true;
+
+    //y
+    public bool up = true, down = false;
+
+    //levitaion
+    public float levitateTimer = 0f;
+    public float floatSpeed = 1.25f;
     void Start()
     {
-        
+        nagaRB = GetComponent<Rigidbody2D>();
+        nagaAnim = GetComponent<Animator>();
+        Target = GameObject.Find("Bulan");
+        TargetPos = new Vector2(Target.transform.position.x, Target.transform.position.y);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        TargetPos = new Vector2(Target.transform.position.x, Target.transform.position.y); //Debug.Log(TargetPos);   
+
+
+        Levitator();
+        UpdateAnimation();
     }
+
+    private void Levitator()
+    {
+        if (levitateTimer <= 10f)
+        {
+            levitateTimer += Time.deltaTime;
+        }
+
+        if (levitateTimer >= 0f && levitateTimer <= 2f)
+        {
+            //Naga goes up
+            nagaRB.transform.position = new Vector2(nagaRB.transform.position.x, nagaRB.transform.position.y + Time.deltaTime);//+ (floatSpeed + Time.deltaTime));
+        }
+
+        if (levitateTimer >= 2.1f && levitateTimer <= 4f)
+        {
+            //Naga goes down
+            nagaRB.transform.position = new Vector2(nagaRB.transform.position.x, nagaRB.transform.position.y - Time.deltaTime);//+ (-floatSpeed + Time.deltaTime));
+        }
+
+        if (levitateTimer > 4f) levitateTimer = 0f;
+
+    }
+    private void UpdateAnimation()
+    {
+        if (nagaRB.transform.position.x < Target.transform.position.x && faceDirection)
+        {
+            faceDirection = !faceDirection;
+            transform.Rotate(0f, 180f, 0f);
+        }
+        else if (nagaRB.transform.position.x > Target.transform.position.x && !faceDirection)
+        {
+            faceDirection = !faceDirection;
+            transform.Rotate(0f, 180f, 0f);
+        }
+    }
+
 }
