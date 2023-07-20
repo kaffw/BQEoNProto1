012b69c7c11d42e46a9df48791edb41fa9f19445
kaffw <coroneljm01@gmail.com>
2023-07-21 04:18:36 +0800

    Naga
    Naga wi background

---
+++ Terrain/Assets/NagaMid.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NagaMid : MonoBehaviour
+{
+    public GameObject dynamicBackground;
+    public Rigidbody2D dynamicBackgroundRB;
+    public GameObject Target;
+    void Start()
+    {
+        Target = GameObject.Find("Bulan");
+        dynamicBackgroundRB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        transform.position = new Vector2(Target.transform.position.x / 7, 10);
+    }
+}
