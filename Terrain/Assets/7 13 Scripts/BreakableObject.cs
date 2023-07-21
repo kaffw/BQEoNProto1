7c417368a5bd4fcb54d58f9c9c954abf48fd35ac
kaffw <coroneljm01@gmail.com>
2023-07-21 21:02:42 +0800

    breakables
    breakables

--- Terrain/Assets/7 13 Scripts/BreakableObject.cs
+++ Terrain/Assets/7 13 Scripts/BreakableObject.cs
@@ -5,25 +5,40 @@ using UnityEngine;
 public class BreakableObject : MonoBehaviour
 {
     public GameObject brokenbit;
-    void OntriggerEnter2D(Collider2D other)
+
+    public GameObject FixedCrate;
+
+    public GameObject drop;
+    public Vector2 Target;
+
+    void Start()
     {
-        if (other.tag == "Projectile")
-            BreakIt();
+        Target = new Vector2(brokenbit.transform.position.x, FixedCrate.transform.position.y);
+
+        Target = new Vector2(FixedCrate.transform.position.x, FixedCrate.transform.position.y);
     }
+
     void OnCollisionEnter2D(Collision2D col)
     {
+
         if (col.collider.tag == "Projectile")
         {
             BreakIt();
         }
+
     }
     public void BreakIt()
     {
         Destroy(this.gameObject);
-        GameObject broke = (GameObject)Instantiate(brokenbit, transform.position, Quaternion.identity);
+        GameObject broke = (GameObject)
+        Instantiate(brokenbit, Target + new Vector2(1, 3), Quaternion.identity);
+        Instantiate(drop, Target + new Vector2(1, 3), Quaternion.identity);
+
         foreach (Transform child in broke.transform)
         {
             child.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-2f, 2f), Random.Range(3f, 7f));
+            drop.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-2f, 2f), Random.Range(3f, 7f));
         }
+
     }
 }
