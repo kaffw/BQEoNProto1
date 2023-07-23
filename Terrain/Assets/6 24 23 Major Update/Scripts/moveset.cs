99aa47ac7ab4c76a29e316db5c3d94fa0c66f9d3
kaffw <coroneljm01@gmail.com>
2023-07-23 12:20:46 +0800

    lava
    lavaa

--- Terrain/Assets/6 24 23 Major Update/Scripts/moveset.cs
+++ Terrain/Assets/6 24 23 Major Update/Scripts/moveset.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
