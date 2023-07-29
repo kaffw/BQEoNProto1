c3bf0d8592104fd321cb9ec41c77ed6c30a140c5
kaffw <coroneljm01@gmail.com>
2023-07-29 14:18:53 +0800

    mp4updated
    yes

--- Terrain/Assets/Scripts/OpeningSceneManager.cs
+++ Terrain/Assets/Scripts/OpeningSceneManager.cs
@@ -5,27 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class OpeningSceneManager : MonoBehaviour
 {
-    public GameObject FirstVid, SecondVid;
+    public GameObject FirstVid, SecondVid, ThirdVid;
     public float timer = 0f;
     private void Start()
     {
         SecondVid.SetActive(false);
+        FirstVid.SetActive(false);
+        ThirdVid.SetActive(true);
     }
 
     void Update()
     {
         timer += Time.deltaTime;
-
-        if (timer > 7.7f)
+        if (timer > 15)
         {
+            ThirdVid.SetActive(false);
+            FirstVid.SetActive(true);
+        }
+        if (timer > 22.7f)
+        {
+            ThirdVid.SetActive(false);
+            FirstVid.SetActive(false);
             SecondVid.SetActive(true);
             
         }
-        if(timer > 8f)
+        /*if(timer > 23f)
         {
+            ThirdVid.SetActive(false);
             FirstVid.SetActive(false);
-        }
-        if (timer > 14.5f)
+            SecondVid.SetActive(true);
+        }*/
+        if (timer > 29.5f)
         {
             SceneManager.LoadScene(21);
         }
